d81b0a4e4c95ace830d9b4a49852bb3f9b3cccb8
MCTGyt <75358424+MCTGyt@users.noreply.github.com>
2022-03-11 11:56:46 +0800

    Update ffmpeg-helper.py

--- ffmpeg-helper.py
+++ ffmpeg-helper.py
@@ -8,8 +8,18 @@ top.title("ffmpeg助手")
 top.geometry('500x300')
 
 def start():
+    if e2.get() != "" :
+        if e4.get() != "" :
+            if e3.get() != "" :
+                os.system("ffmpeg" +" " + "-i" + " " + e2.get() + " " + e4.get() + " " + e3.get())
+            else :
+                print("你未输入资料")
+        else :
+            print("你未输入资料")
+    else :
+        print("你未输入资料")
     #tkinter.messagebox.showinfo(title='Start', message='开始压缩，自定义选项将在下个版本开放')
-    os.system("ffmpeg" +" " + "-i" + " " + e2.get() + " " + e4.get() + " " + e3.get())
+
 
 def open_blog(event):
     webbrowser.open_new_tab("https://mctg.ink")
@@ -24,7 +34,6 @@ l5 = tkinter.Label(top, text="自定义参数 例如 -s vga*")
 
 l4 = tkinter.Label(top, text="by 天哥de博客", fg="blue")
 l4.bind("<Button-1>", open_blog)
-
 
 l1.pack() #源文件
 e2.pack() #源文件input
